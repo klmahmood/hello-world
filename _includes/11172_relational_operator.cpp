2da40efcd262f091cd7d0e044b78804f27e5f249
Khalid Mahmood <Khalid@ITC-Zen-10116-068106E6-0DEE-5A0D-BE08-37628C97A1B4.local>
2018-05-18 11:05:47 +0200

    cpp file modified

--- _includes/11172_relational_operator.cpp
+++ _includes/11172_relational_operator.cpp
@@ -1,25 +1,19 @@
 //*************************************************************************************//
-//        Problem : 11172	Relational Operator
-//        Language: ANSI C
-//        Comment  : Very easy, Ad hoc 
-// 		  Status   : AC
+//        Problem  : 11172 - Relational Operator
+//        Language : ANSI C
+//        Comment  : Very easy, Ad hoc
+// 	  Status   : AC
 //*************************************************************************************//
 
 #include <stdio.h>
-int main()
-{
-
-	int TC, a, b;
-	scanf("%d", &TC);
-	while(TC--)
-	{
-		scanf("%d %d", &a, &b);
-		if(a<b)
-			printf("<\n");
-		if(a>b)
-			printf(">\n");
-		if(a==b)
-			printf("=\n");
-	}
-	return 0;
+int main() {
+  int TC, a, b;
+  scanf("%d", &TC);
+  while (TC--) {
+    scanf("%d %d", &a, &b);
+    if (a < b) printf("<\n");
+    if (a > b) printf(">\n");
+    if (a == b) printf("=\n");
+  }
+  return 0;
 }
